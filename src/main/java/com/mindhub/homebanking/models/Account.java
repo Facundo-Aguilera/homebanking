7bcd96214af8a1fe2be5a0cb7282ccce1609c89c
Facundo-Aguilera <102349161+Facundo-Aguilera@users.noreply.github.com>
2023-09-01 01:10:07 -0300

    Task 8
    Transactions

--- src/main/java/com/mindhub/homebanking/models/Account.java
+++ src/main/java/com/mindhub/homebanking/models/Account.java
@@ -2,13 +2,11 @@ package com.mindhub.homebanking.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.GenericGenerator;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 import java.time.LocalDate;
-import javax.persistence.ManyToOne;
-import javax.persistence.FetchType;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -17,52 +15,51 @@ public class Account {
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
     private String number;
-    private LocalDate creationDate;
+    private LocalDate date;
     private Double balance;
-
     @ManyToOne(fetch = FetchType.EAGER)
     private Client client;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "account")
+    private Set<Transaction> transactions = new HashSet<>();
 
     public Account() { }
-
-    public Account(String number, LocalDate creationDate, Double balance) {
+    public Account(String number, LocalDate date, Double balance) {
         this.number = number;
-        this.creationDate = creationDate;
+        this.date = date;
         this.balance = balance;
     }
 
     public Long getId() {
         return id;
     }
-
     public String getnumber() {
         return number;
     }
-
     public void setnumber(String number) {
         this.number = number;
     }
-
-    public LocalDate getcreationDate() {
-        return creationDate;
+    public LocalDate getDate() {
+        return date;
     }
-
-    public void setcreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
-
     public Double getbalance() {
         return balance;
     }
-
     public void setbalance(Double balance) { this.balance = balance; }
-
-    @JsonIgnore
     public Client getClient() {
         return client;
     }
-
     public void setClient(Client client) {
         this.client = client;
+    }
+
+    public void addTransaction(Transaction transaction){
+        transaction.setAccount(this);
+        this.transactions.add(transaction);
+    }
+    public Set<Transaction> getTransactions() {
+        return transactions;
     }
 }
--- src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
@@ -1,10 +1,13 @@
 package com.mindhub.homebanking.repositories;
-import java.util.List;
 
 import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Boolean existsByNumber(String number);
+    Account findByNumber(String number);
+    Boolean existsByNumberAndClient(String number, Client client);
 }
